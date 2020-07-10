cd2f4fc90bf4bcb8824adcc4a63498e2ef4f5746
Max Naumov <41187479+diastor@users.noreply.github.com>
2020-07-10 16:06:42 +0500

    Добавьте файлы проекта.

---
+++ pdfStamp/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace pdfStamp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.selectFile = new System.Windows.Forms.TextBox();
+            this.SaveFileBtn = new System.Windows.Forms.Button();
+            this.outDerectory = new System.Windows.Forms.TextBox();
+            this.numStamp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameFile = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(267, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ОК";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Выбрать файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // selectFile
+            // 
+            this.selectFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.selectFile.Location = new System.Drawing.Point(149, 16);
+            this.selectFile.Name = "selectFile";
+            this.selectFile.ReadOnly = true;
+            this.selectFile.Size = new System.Drawing.Size(207, 26);
+            this.selectFile.TabIndex = 4;
+            // 
+            // SaveFileBtn
+            // 
+            this.SaveFileBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SaveFileBtn.Location = new System.Drawing.Point(12, 53);
+            this.SaveFileBtn.Name = "SaveFileBtn";
+            this.SaveFileBtn.Size = new System.Drawing.Size(131, 31);
+            this.SaveFileBtn.TabIndex = 5;
+            this.SaveFileBtn.Text = "Сохранить";
+            this.SaveFileBtn.UseVisualStyleBackColor = true;
+            this.SaveFileBtn.Click += new System.EventHandler(this.SaveFileBtn_Click);
+            // 
+            // outDerectory
+            // 
+            this.outDerectory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outDerectory.Location = new System.Drawing.Point(149, 55);
+            this.outDerectory.Name = "outDerectory";
+            this.outDerectory.ReadOnly = true;
+            this.outDerectory.Size = new System.Drawing.Size(207, 26);
+            this.outDerectory.TabIndex = 6;
+            // 
+            // numStamp
+            // 
+            this.numStamp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numStamp.Location = new System.Drawing.Point(191, 103);
+            this.numStamp.Name = "numStamp";
+            this.numStamp.Size = new System.Drawing.Size(97, 26);
+            this.numStamp.TabIndex = 7;
+            this.numStamp.TextChanged += new System.EventHandler(this.numStamp_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Номер";
+            // 
+            // nameFile
+            // 
+            this.nameFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameFile.Location = new System.Drawing.Point(114, 137);
+            this.nameFile.Name = "nameFile";
+            this.nameFile.Size = new System.Drawing.Size(242, 26);
+            this.nameFile.TabIndex = 9;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Имя файла";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(114, 173);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(242, 26);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Дата";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(110, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Идет выполнение";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(77, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Вх. № 01/101-";
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPlus.Location = new System.Drawing.Point(328, 101);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(28, 30);
+            this.btnPlus.TabIndex = 16;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnMinus.Location = new System.Drawing.Point(294, 101);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(28, 30);
+            this.btnMinus.TabIndex = 17;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 255);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nameFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numStamp);
+            this.Controls.Add(this.outDerectory);
+            this.Controls.Add(this.SaveFileBtn);
+            this.Controls.Add(this.selectFile);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Устанока штампа";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox selectFile;
+        private System.Windows.Forms.Button SaveFileBtn;
+        private System.Windows.Forms.TextBox outDerectory;
+        private System.Windows.Forms.TextBox numStamp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameFile;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+    }
+}
+
---
+++ pdfStamp/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+namespace pdfStamp
+{
+    public partial class Form1 : Form
+    {
+
+        string filePath = string.Empty;
+        string filePathOut = string.Empty;
+        string numberInStamp = string.Empty;
+        string currentDate = string.Empty;
+        string fileName = string.Empty;
+        string fileNameOut = string.Empty;
+
+        
+        Dictionary<string, string> Month = new Dictionary<string, string>
+        {
+            {"1", "января"},
+            {"2", "февраля"},
+            {"3", "марта"},
+            {"4", "апреля"},
+            {"5", "мая"},
+            {"6", "июня"},
+            {"7", "июля"},
+            {"8", "августа"},
+            {"9", "сентября"},
+            {"10", "октября"},
+            {"11", "ноября"},
+            {"12", "декабря"},
+        };
+    
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.ForeColor = Color.Red;
+            label4.Text = "Идет выполнение";
+            numberInStamp = numStamp.Text;
+            
+            
+            //if (outDerectory.Text == "")
+            //{
+            //    filePathOut = filePath.Remove(filePath.LastIndexOf('\\'));
+            //}
+
+            button1.Enabled = false;
+            label4.Visible = true;
+
+            
+            DateTime dt = dateTimePicker1.Value;//Получение текущей даты 
+            string month = string.Empty;
+            if(dt.Month < 10)
+            {
+                month = "0" + dt.Month.ToString();
+            }
+            else
+            {
+                month = dt.Month.ToString();
+            }
+
+            //Устанавливаем номер и дату в печать
+            // old "C:\\distr\\blueStampMini.png -fill Black -pointsize 18 -draw \"gravity SouthEast text 100,98  '01/101-" + numberInStamp + "'\" -draw \"gravity SouthEast text 255,72 '" + dt.Day.ToString() + "'\" -draw \"gravity SouthEast text 155,72 '" + month + "'\" -draw \"gravity SouthEast text 60,72 '" + dt.Year%2000 + "'\" C:\\distr\\completedStamp.png";
+            string convStampArgs = "C:\\distr\\blueStampMini.png +contrast -fill Black -pointsize 14 -weight Bold -draw \"gravity SouthEast text 50,60  '01/101-" + numberInStamp + "'\" -draw \"gravity SouthEast text 150,42 '" + dt.Day.ToString() + "'\" -draw \"gravity SouthEast text 95,42 '" + month + "'\" -draw \"gravity SouthEast text 35,42 '" + dt.Year%2000 + "'\" C:\\distr\\completedStamp.png";
+            Process convetrStamp = new Process();
+            convetrStamp.StartInfo.FileName = "C:\\distr\\im\\convert.exe";
+            convetrStamp.StartInfo.Arguments = convStampArgs;
+            convetrStamp.StartInfo.UseShellExecute = false;
+            convetrStamp.StartInfo.RedirectStandardOutput = true;
+            convetrStamp.StartInfo.CreateNoWindow = true;
+            convetrStamp.Start();
+            convetrStamp.WaitForExit();
+            if (convetrStamp.ExitCode !=0)
+            {
+                int errorCod = convetrStamp.ExitCode;
+                MessageBox.Show("convetrStamp"+errorCod.ToString());
+            }
+            convetrStamp.Close();
+
+            string convStampOnPDF = "-composite -density  120 -quality 200 " + filePath+ "[0]" + " C:\\distr\\completedStamp.png -gravity southeast -geometry +100+100 c:\\distr\\" + "stampedPage.pdf";
+            Process stampOnPDF = new Process();
+            stampOnPDF.StartInfo.FileName = "C:\\distr\\im\\convert.exe";
+            stampOnPDF.StartInfo.Arguments = convStampOnPDF;
+            stampOnPDF.StartInfo.UseShellExecute = false;
+            stampOnPDF.StartInfo.RedirectStandardOutput = true;
+            stampOnPDF.StartInfo.CreateNoWindow = true;
+            stampOnPDF.Start();
+            stampOnPDF.WaitForExit();
+            if (stampOnPDF.ExitCode != 0)
+            {
+                int errorCod = stampOnPDF.ExitCode;
+                MessageBox.Show("stampOnPDF"+errorCod.ToString());
+            }
+            stampOnPDF.Close();
+
+            string convFullPdf = "--overlay  c:\\distr\\stampedPage.pdf" + " --from=1 --to=1 -- " + filePath + " " +Properties.Settings.Default.defaultPathOut + "\\"+ "Вх_№_01_101_" + Properties.Settings.Default.stumpNum + ".pdf";
+
+            Process fullPdf = new Process();
+            fullPdf.StartInfo.FileName = "C:\\distr\\qpdf\\bin\\qpdf.exe";
+            fullPdf.StartInfo.Arguments = convFullPdf;
+            fullPdf.StartInfo.UseShellExecute = false;
+            fullPdf.StartInfo.RedirectStandardOutput = true;
+            fullPdf.StartInfo.CreateNoWindow = true;
+            fullPdf.Start();
+            fullPdf.WaitForExit();
+            if (fullPdf.ExitCode != 0)
+            {
+                int errorCod = fullPdf.ExitCode;
+                MessageBox.Show("fullPdf" + errorCod.ToString());
+            }
+            fullPdf.Close();
+
+
+            label4.ForeColor = Color.Green;
+            label4.Text = "Готово";
+
+            //System.IO.File.Delete("C:\\distr\\completedStamp.png");
+            //System.IO.File.Delete("C:\\distr\\stampedPage.pdf");
+            button1.Enabled = true;
+            Properties.Settings.Default.stumpNum++;
+            Properties.Settings.Default.Save();
+            numStamp.Text = Properties.Settings.Default.stumpNum.ToString();
+            nameFile.Text = "Вх_№_01_101_" + Properties.Settings.Default.stumpNum.ToString();
+
+        }
+
+        //Вызов окна выбора файла
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "pdf files (*.pdf)|*.pdf";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+            selectFile.Text = filePath;
+            //filePathOut = filePath.Remove(filePath.LastIndexOf('\\'));
+            //outDerectory.Text = filePathOut;
+            //Активируем кнопку "старт", при запуске неактивна
+            if (filePath != "")
+            {
+                button1.Enabled = true;
+            }
+            fileName = Path.GetFileName(filePath);
+            nameFile.Text = "Вх_№_01_101_" + Properties.Settings.Default.stumpNum;
+
+        }
+
+        //Окно выбора места сохранения сайта
+        private void SaveFileBtn_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePathOut = dialog.SelectedPath;
+                }
+            outDerectory.Text = filePathOut;
+            Properties.Settings.Default.defaultPathOut = filePathOut;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime date = DateTime.Now;
+            currentDate = date.ToShortDateString();
+            //кнопка старт выключена по умолчанию 
+            button1.Enabled = false;
+            label4.Visible = false;
+            //устанавливаем номер печати из settings
+            numStamp.Text = Properties.Settings.Default.stumpNum.ToString();
+            outDerectory.Text = Properties.Settings.Default.defaultPathOut.ToString();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F2 && e.Shift)
+            {
+                settings settingsForm = new settings();
+                settingsForm.Show();
+                MessageBox.Show("JR");
+            }
+
+        }
+        
+        private void numStamp_TextChanged(object sender, EventArgs e)
+        {
+            nameFile.Clear();
+            nameFile.Text = "Вх_№_01_101_"+numStamp.Text.Replace(" ","_").Replace("/","_");
+            fileNameOut = nameFile.Text;
+            fileNameOut = fileNameOut.Replace(" ", "_").Replace("/", "_");
+
+            if (numStamp.Text != "")
+            {
+               Properties.Settings.Default.stumpNum = Convert.ToInt32(numStamp.Text);
+            }
+    
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+           Properties.Settings.Default.stumpNum++;
+           numStamp.Text = Properties.Settings.Default.stumpNum.ToString();
+           Properties.Settings.Default.Save();
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.stumpNum !=0)
+            {
+                Properties.Settings.Default.stumpNum--;
+                numStamp.Text = Properties.Settings.Default.stumpNum.ToString();
+                Properties.Settings.Default.Save();
+            }
+        }
+    }
+}
